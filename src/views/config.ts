620968e1c82018aaf00a18817761e76d3c47ba29
Guitenbay <gtenbay@163.com>
2020-04-26 15:24:45 +0800

    修改 html title

--- src/views/config.ts
+++ src/views/config.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// const baseURL = 'http://www.biki.wiki:3000';
+// const fusekiURL = 'http://www.biki.wiki:8000/fuseki';
+
 const baseURL = 'http://localhost:3000';
 const fusekiURL = 'http://localhost:8000/fuseki';
 const Axios = axios.create({
