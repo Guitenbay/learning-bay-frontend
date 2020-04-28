9bc4f278aaac2208c2dbf394ecb219dec80fab60
Guitenbay <gtenbay@163.com>
2020-04-29 00:52:14 +0800

    设置阿里云服务器IP地址

--- src/views/config.ts
+++ src/views/config.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// const baseURL = 'http://139.196.179.105:3000';
-// const fusekiURL = 'http://139.196.179.105:8000/fuseki';
+const baseURL = 'http://139.196.179.105:3000';
+const fusekiURL = 'http://139.196.179.105:8000/fuseki';
 
-const baseURL = 'http://localhost:3000';
-const fusekiURL = 'http://localhost:8000/fuseki';
+// const baseURL = 'http://localhost:3000';
+// const fusekiURL = 'http://localhost:8000/fuseki';
 
 const aliyunOSSURL = 'https://learningbay-20200428.oss-cn-shanghai.aliyuncs.com'
 const Axios = axios.create({
